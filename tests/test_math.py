99b5cf2d903dacaf7b44c04164b21f2d7819b840
kdbasema <kdbasema@iastate.edu>
2017-07-25 16:54:53 -0400

    Made changes to math_test file

--- tests/test_math.py
+++ tests/test_math.py
@@ -2,13 +2,13 @@
 File for testing the math.py module
 """
 
-import bug_free_robot
+import bug_free_robot as bfr
 import pytest
 
 
 def test_add():
-    assert bug_free_robot.add(5, 2) == 7
-    assert bug_free_robot.add(3, 8) == 11
+    assert bfr.math.add(5, 2) == 7
+    assert bfr.math.add(3, 8) == 11
 
 
 testdata  = [
@@ -18,32 +18,32 @@ testdata  = [
     (11, 0, 0),
     (0, 0, 0),
 ]
-@pytest.mark.parameterize("a,b,expected", testdata)
+@pytest.mark.parametrize("a,b,expected", testdata)
 def test_mult(a, b, expected):
-    assert bug_free_robot.mult(a, b) == expected
-    assert bug_free_robot.mult(b, a) == expected
+    assert bfr.math.mult(a, b) == expected
+    assert bfr.math.mult(b, a) == expected
 
 
 def test_div():
-    assert bug_free_robot.div(5, 5) == 1
-    assert bug_free_robot.div(9, 3) == 3
+    assert bfr.math.div(5, 5) == 1
+    assert bfr.math.div(9, 3) == 3
 
 
 def test_mult():
-    assert bug_free_robot.subtract(5, 2) == 3
-    assert bug_free_robot.subtract(3, 8) == -5
+    assert bfr.math.subtract(5, 2) == 3
+    assert bfr.math.subtract(3, 8) == -5
    
 
 def test_power():
-    assert bug_free_robot.power(2, 3) == 8
-    assert bug_free_robot.power(3, 3) == 27
+    assert bfr.math.power(2, 3) == 8
+    assert bfr.math.power(3, 3) == 27
 
 
 def test_degtorad():
-    assert bug_free_robot.degtorad(45) == 3.14159 / 4
-    assert bug_free_robot.degtorad(180) == 3.14159
+    assert bfr.math.degtorad(45) == 3.14159 / 4
+    assert bfr.math.degtorad(180) == 3.14159
 
 
 def test_radtodeg():
-    assert bug_free_robot.radtodeg(3.14159) == 180
-    assert bug_free_robot.radtodeg(3.14159 * 0.5) == 90
+    assert bfr.math.radtodeg(3.14159) == 180
+    assert bfr.math.radtodeg(3.14159 * 0.5) == 90
