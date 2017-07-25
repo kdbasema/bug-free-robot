dc22ab3c47aac38e04959c0631a090d4b8f5da0b
kdbasema <kdbasema@iastate.edu>
2017-07-25 15:05:00 -0400

    Added mult test

--- tests/test_math.py
+++ tests/test_math.py
@@ -9,3 +9,8 @@ import pytest
 def test_add():
     assert bug_free_robot.add(5, 2) == 7
     assert bug_free_robot.add(3, 8) == 11
+
+
+def test_mult():
+    assert bug_free_robot.mult(5, 2) == 10
+    assert bug_free_robot.mult(3, 8) == 24
