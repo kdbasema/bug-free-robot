45e1ad9d0e78c0633099c0be1f01d16adef51d05
kdbasema <kdbasema@iastate.edu>
2017-07-25 14:28:53 -0400

    Wrote math functions in python script labeled math

---
+++ bug_free_robot/__init__.py
@@ -0,0 +1,6 @@
+"""
+THis is the base file of the bug-free-robot
+"""
+
+from . import math
+from .math import *
---
+++ bug_free_robot/math.py
@@ -0,0 +1,53 @@
+"""
+A small set of functions for doing math operations
+"""
+
+
+# Write a function named add that adds two values
+def add(a, b):
+    """
+    Function that returns the sum of a and b
+    """
+    return a + b
+
+
+def mult(a, b):
+    """
+    Function that returns the product of a and b
+    """
+    return a * b
+
+
+def div(a, b):
+    """
+    Function that returns the fraction of a over b
+    """
+    return a / b
+
+
+def subtract(a, b):
+    """
+    Function that returns the diference of a and b
+    """
+    return a - b
+
+
+def power(a, p):
+    """
+    Function that returns a to the p
+    """
+    return a ** p
+
+
+def radtodeg(rad):
+    """
+    Function that converts radians to degrees
+    """
+    return rad * (180/3.14159)
+
+
+def degtorad(deg):
+    """
+    Function that converst radians to degrees
+    """
+    return deg * (3.14159/180)
