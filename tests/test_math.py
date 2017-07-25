5f34fc3a0385fa383ece454b440650ce7cdadae6
kdbasema <kdbasema@iastate.edu>
2017-07-25 15:38:15 -0400

    Parameterized test data for math.mult()

--- tests/test_math.py
+++ tests/test_math.py
@@ -10,10 +10,17 @@ def test_add():
     assert bug_free_robot.add(5, 2) == 7
     assert bug_free_robot.add(3, 8) == 11
 
-
-def test_mult():
-    assert bug_free_robot.mult(5, 2) == 10
-    assert bug_free_robot.mult(3, 8) == 24
+testdata = [
+    (2, 5, 10),
+    (1, 2, 2),
+    (11, 9, 99),
+    (11, 0 , 0),
+    (0, 0, 0),
+]
+@pytest.mark.parametrize("a,b,expected", testdata)
+def test_mult(a, b, expected):
+    assert bug_free_robot.mult(a, b) == expected
+    assert bug_free_robot.mult(a, a) == expected
 
 
 def test_div():
